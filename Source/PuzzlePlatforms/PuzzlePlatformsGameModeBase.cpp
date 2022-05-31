bb9cd45964e4707b7f7841ee546501dcbc264af3
Hello3angle <hello.3angle@gmail.com>
2022-05-31 16:05:13 +1000

    fix c++ gamemode bug

--- Source/PuzzlePlatforms/PuzzlePlatformsGameModeBase.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameModeBase.cpp
@@ -6,9 +6,10 @@
 APuzzlePlatformsGameModeBase::APuzzlePlatformsGameModeBase()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+	DefaultPawnClass = PlayerPawnBPClass.Class;*/
 }
